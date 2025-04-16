a3668ea412782923458e3291a1f12363ee5f777c
jack slavick <jslavick8411@stu.d214.org>
2025-04-16 08:06:02 -0700

    jn

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,5 @@
-import SwiftUI
+import SpriteKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-    }
+class GameScene: SKScene {
+    
 }
