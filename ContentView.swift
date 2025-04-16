02c3ef2bd13e32a72b4a2ccd848763e0395cd843
jack slavick <jslavick8411@stu.d214.org>
2025-04-16 08:11:10 -0700

    s

--- ContentView.swift
+++ ContentView.swift
@@ -2,4 +2,14 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    var ground: SKSpriteNode!
+    
+    
+    override func didMove(to view:SKView) {
+        
+        player = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        player.position = CGPoint(x:self.frame.midX, y: self.frame.midY)
+        addChild(player)
+    }
 }
