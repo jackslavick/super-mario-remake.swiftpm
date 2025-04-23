b2feba4742503b5e5f06e8204f0c1517b41222fe
jack slavick <jslavick8411@stu.d214.org>
2025-04-23 08:53:05 -0700

    n

--- ContentView.swift
+++ ContentView.swift
@@ -5,22 +5,28 @@ class GameScene: SKScene {
     var player: SKSpriteNode!
     var ground: SKSpriteNode!
     
+   
+    override init(size: CGSize) {
+        super.init(size: size)
+        self.scaleMode = .aspectFill
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func didMove(to view: SKView) {
-        
         player = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
         player.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
         addChild(player)
         
-      
         ground = SKSpriteNode(color: .red, size: CGSize(width: self.frame.width, height: self.frame.height))
         ground.position = CGPoint(x: self.frame.midX, y: -self.frame.height / 2 + 25)
         addChild(ground)
         
-       
         self.physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
     }
-    
     
     func movePlayer(to direction: String) {
         guard let player = player else { return }
@@ -39,7 +45,6 @@ class GameScene: SKScene {
         player.run(moveAction)
     }
     
-   
     func jumpPlayer() {
         if player.physicsBody?.velocity.dy == 0 {
             player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 250))
--- MyApp.swift
+++ MyApp.swift
@@ -1,15 +1,18 @@
 import SwiftUI
 import SpriteKit
 
-
 struct ContentView: View {
-    @State private var gameScene = GameScene(size: CGSize(width: 400, height: 600))
+    @State private var gameScene: GameScene
+    
+   
+    init() {
+        _gameScene = State(initialValue: GameScene(size: CGSize(width: 400, height: 600)))
+    }
 
     var body: some View {
         VStack {
             SpriteView(scene: gameScene)
                 .frame(width: 300, height: 400)
-            
             
             HStack {
                 Button("Move Left") {
@@ -17,17 +20,16 @@ struct ContentView: View {
                 }
                 
                 Button("Move Right") {
-                    gameScene.movePlayer(to: "right") 
+                    gameScene.movePlayer(to: "right")
                 }
             }
             .padding()
         }
-    }
         .onAppear {
             let scene = GameScene(size: CGSize(width: 300, height: 400))
             scene.scaleMode = .aspectFill
             self.gameScene = scene
-            }
         }
-
+    }
+}
 
