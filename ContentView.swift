ad7e033f46814a52839edd5c7517411daeb0c2c8
jack slavick <jslavick8411@stu.d214.org>
2025-05-13 08:33:02 -0700

    w

--- ContentView.swift
+++ ContentView.swift
@@ -1,15 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+    
     @State private var marioPosition = CGPoint(x: 200, y: 500)
     @State private var marioVelocity = CGSize(width: 0, height: 0)
     @State private var isJumping = false
     @State private var isOnGround = false
     
-   
     let platform = CGRect(x: 0, y: 600, width: 400, height: 50)
-    
     let gravity: CGFloat = 0.5
     let jumpForce: CGFloat = -15
     let moveSpeed: CGFloat = 5
@@ -17,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             ZStack {
-               
+                
                 Color.blue.edgesIgnoringSafeArea(.all)
                 
                
@@ -26,48 +24,55 @@ struct ContentView: View {
                     .position(x: platform.midX, y: platform.midY)
                     .foregroundColor(.green)
                 
-               
-                Rectangle()
+                
+                Image(systemName: "mario")
+                    .resizable()
                     .frame(width: 50, height: 50)
                     .position(marioPosition)
-                    .foregroundColor(.red)
+                    .scaledToFit()
             }
             .gesture(DragGesture(minimumDistance: 0)
                 .onChanged { value in
-                   
                     if value.translation.width > 0 {
-                       
                         marioPosition.x += moveSpeed
                     } else if value.translation.width < 0 {
-                      
                         marioPosition.x -= moveSpeed
                     }
                 }
             )
-            .onAppear {
-                startGameLoop()
+            .onTapGesture {
+                if isOnGround {
+                    marioVelocity.height = jumpForce
+                    isOnGround = false
+                }
             }
-        }
-    }
-    
-    
-    func startGameLoop() {
-        Timer.scheduledTimer(withTimeInterval: 0.016, repeats: true) { _ in
-          
-           
+            .onAppear {
+                
+            }
         }
     }
     
     
     func applyGravity() {
-       
+        
         if !isOnGround {
             marioVelocity.height += gravity
         } else {
             marioVelocity.height = 0
         }
+    }
+    
+    func updatePosition() {
+       
+        marioPosition.y += marioVelocity.height
         
-        
+      
+        if marioPosition.y >= platform.minY - 25 {
+            marioPosition.y = platform.minY - 25
+            isOnGround = true
+        } else {
+            isOnGround = false
+        }
     }
 }
 
@@ -79,3 +84,4 @@ struct MarioGameApp: App {
         }
     }
 }
+
